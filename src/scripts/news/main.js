517fc2b79b9b4a4cc53b069bcb9c4265591efd63
Chris Cotton <christopher.cotton88@gmail.com>
2020-05-22 14:04:57 -0500

    last one

--- src/scripts/news/main.js
+++ src/scripts/news/main.js
@@ -7,21 +7,22 @@ import renderArticles from "./render.js"
 import articleObject from "./main.js"
 
 const recordNewsArticle = () => {
-        const recordNewsButton = document.querySelector("#recordNews--btn");
-        recordNewsButton.addEventListener("click", function () {
-            const urlInput = document.querySelector("#newsURL");
-            const titleInput = document.querySelector("#newsTitle");
-            const synopsisInput = document.querySelector("#newsSynopsis");
-            const timestampInput = document.querySelector("#newsTimestamp");
+    const recordNewsButton = document.querySelector("#recordNews--btn");
+    recordNewsButton.addEventListener("click", function () {
+        const urlInput = document.querySelector("#newsURL");
+        const titleInput = document.querySelector("#newsTitle");
+        const synopsisInput = document.querySelector("#newsSynopsis");
+        const timestampInput = document.querySelector("#newsTimestamp");
 
-             const newArticle = articleObject(urlInput.value, titleInput.value, synopsisInput.value, timestampInput.value);
+        const newArticle = articleObject(urlInput.value, titleInput.value, synopsisInput.value, timestampInput.value);
 
-                newsApi.recordNewsArticle(newArticle)
-                    .then(newsApi.getAllArticles).then(renderArticles)
+        newsApi.recordNewsArticle(newArticle)
+            .then(newsApi.getAllArticles).then(renderArticles)
 
-            newsApi.getAllArticles().then(renderArticles)
-        
-        }
+        newsApi.getAllArticles().then(renderArticles)
 
+    })
+
+}
 
 export default recordNewsArticle
